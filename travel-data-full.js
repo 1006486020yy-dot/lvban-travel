f53fffbcd6ff2abc99d4bda2962b5bcb99888df1
1006486020yy-dot <1006486020yy@gmail.com>
2026-08-12 16:10:55 +0800

    导入用户提供的全国旅游数据库完整数据

---
+++ travel-data-full.js
@@ -0,0 +1,2 @@
+/* 全国旅游数据库 · 用户提供扩充版表格完整数据：cities / spots / foods / hotels */
+window.LVBAN_DATA="+json.dumps({"cities":cities,"spots":spots,"foods":foods,"hotels":hotels},ensure_ascii=False,separators=(',',':'))+";
